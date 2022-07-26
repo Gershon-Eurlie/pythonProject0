96fa4399fb2c55230c65557e3e567e7d6695e6ba
Gershon-Eurlie <genormenyo@st.knust.edu.gh>
2022-07-26 14:30:44 -0700

    The initial commit. Just a simple of python classes

---
+++ trial.py
@@ -0,0 +1,17 @@
+#def sqsum(nums):
+#    return sum(x**2 for x in nums if x>0)
+
+class FunEvent:
+    def __init__(self, tags, year):
+        self.tags = tags
+        self.year = year
+
+    def __str__(self):
+        return f"FunEvent(tags={self.tags}, year={self.year})"
+
+tags = ["google", "ml"]
+year = 2022
+bootcamp = FunEvent(tags, year)
+tags.append("bootcamp")
+year = 2023
+print(bootcamp)
